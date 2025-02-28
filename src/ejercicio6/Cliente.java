d77cf192a7937c630496035a555e16d92cfb9ac7
DiegoYanez1227 <diegoyan0260@gmail.com>
2025-02-28 18:55:39 +0100

    commit

---
+++ src/ejercicio6/Cliente.java
@@ -0,0 +1,41 @@
+package ejercicio6;
+
+import java.net.Socket;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+
+		Scanner sc= new Scanner(System.in);
+		try(Socket socket= new Socket("localhost",5000);
+				InputStream is = socket.getInputStream();
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader br= new BufferedReader(isr);
+
+				OutputStream os = socket.getOutputStream();
+				OutputStreamWriter osw= new OutputStreamWriter(os);
+				PrintWriter pw= new PrintWriter(osw);
+
+				){
+
+			String mensajeRecibido="";
+			
+			while(!((mensajeRecibido=br.readLine()).equalsIgnoreCase("fin"))) {
+				System.out.println("Introduce otra opcion");
+				String mensajeEnviado= sc.nextLine();
+				System.out.println("Cliente envia: "+mensajeEnviado);
+				pw.println(mensajeEnviado);
+				pw.flush();
+			}
+			System.out.println("fin del programa del cliente");
+
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ src/ejercicio6/Servidor.java
@@ -0,0 +1,56 @@
+package ejercicio6;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.io.*;
+
+public class Servidor {
+
+	public static void main(String[] args) {
+		try(
+		ServerSocket server = new ServerSocket(5000);
+		
+		Socket socket = server.accept();
+				
+		InputStream is= socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br= new BufferedReader(isr);
+				
+		OutputStream os= socket.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(os);
+		PrintWriter pw = new PrintWriter(osw);
+		
+		){
+			
+			int numero= (int)Math.random()*10+1;
+			String mensajeRecibido;
+			int numeroRecibido;
+			do {
+				mensajeRecibido=br.readLine();
+				numeroRecibido = Integer.parseInt(mensajeRecibido);
+				if(numeroRecibido > numero) {
+					System.out.println("El numero debe de ser menor");
+					pw.println("El numero debe de ser menor");
+					pw.flush();
+				}else if(numeroRecibido< numero) {
+					System.out.println("El numero debe de ser mayor");
+					pw.println("El numero debe de ser mayor");
+					pw.flush();
+				}else {
+					System.out.println("has acertado");
+					pw.println("fin");
+					pw.flush();
+				}
+			}
+			while(numeroRecibido!=numero);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
